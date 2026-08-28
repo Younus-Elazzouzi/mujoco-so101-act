3ab17280a307e08287ef59a1cc1abce2f361b25a
Younus <younus.elazzouzi@gmail.com>
2026-08-28 03:46:14 +0300

    feat: implement demo pick-and-place

--- demo_pick_and_place.py
+++ demo_pick_and_place.py
@@ -67,6 +67,22 @@ final_configuration_closed = {
     'wrist_roll': 0.0,
     'gripper': 5    
 }
+lift_up_configuration_closed = {
+    'shoulder_pan': -45.0,   # in radians for mujoco! 
+    'shoulder_lift': 0.0,
+    'elbow_flex': 0.0,
+    'wrist_flex': 90.0,
+    'wrist_roll': 0.0,
+    'gripper': 5    
+}
+rotate_configuration_closed = {
+    'shoulder_pan': 45.0,   # in radians for mujoco! 
+    'shoulder_lift': 0.0,
+    'elbow_flex': 0.0,
+    'wrist_flex': 90.0,
+    'wrist_roll': 0.0,
+    'gripper': 5    
+}
 
 
 set_initial_pose(d, starting_configuration)
@@ -79,5 +95,23 @@ with mujoco.viewer.launch_passive(m, d) as viewer:
   # Add cubes as a site for visualization
   show_cubes(viewer, starting_configuration, final_configuration)
   
-  # Hold Starting Position for 2 seconds
-  hold_position(m, d, viewer, 20.0)
+  # Start pick and place
+  move_to_pose(m, d, viewer, starting_configuration_closed, 2.0)
+
+  hold_position(m, d, viewer, 2.0)
+
+  move_to_pose(m, d, viewer, lift_up_configuration_closed, 2.0)
+
+  hold_position(m, d, viewer, 2.0)
+
+  move_to_pose(m, d, viewer, rotate_configuration_closed, 3.0)
+
+  hold_position(m, d, viewer, 2.0)
+
+  move_to_pose(m, d, viewer, final_configuration_closed, 2.0)
+
+  hold_position(m, d, viewer, 2.0)
+
+  move_to_pose(m, d, viewer, final_configuration, 2.0)
+
+  hold_position(m, d, viewer, 2.0)
