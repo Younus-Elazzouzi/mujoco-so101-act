b5fb4d946ba06217de95b2460d00bf8a14f620f3
Younus <younus.elazzouzi@gmail.com>
2026-09-02 08:25:22 +0300

    fix: match tool frame with wrist frame

--- so101_forward_kinematics.py
+++ so101_forward_kinematics.py
@@ -58,7 +58,7 @@ def get_g45(theta5_deg):
 
 def get_g5t():
     displacement = (0.0, 0.0, -0.1034)
-    rotation = Rx(90) @ Rz(270)
+    rotation = Rx(0) # no rotation
     pose = np.block([[rotation, np.array(displacement).reshape(3,1)], [0, 0, 0, 1]])
     return pose
 
